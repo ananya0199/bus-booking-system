49b4a279a0d14c28e050945aca1bd0572fd9d0d6
Ananya <ananya.srivstv@gmail.com>
2023-08-18 17:16:29 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,12 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
+
   res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
   console.log('DB connected successfully');
-  
+
 });
